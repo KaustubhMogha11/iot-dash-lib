d4de201812c216c054ca81f1160becd18ef2aa0b
Your Name <you@example.com>
2020-07-26 17:18:09 +0530

    format documents

--- src/components/chartjs.js
+++ src/components/chartjs.js
@@ -2,33 +2,33 @@ import React from 'react'
 import Chart from 'chart.js'
 import 'chartjs-plugin-colorschemes'
 
-
 export default class ChartJS extends React.Component {
   constructor(props) {
-    super(props);
-    
-    this.canvasRef = React.createRef();
-    this.state  = {
-      data : [] 
-    };
+    super(props)
+
+    this.canvasRef = React.createRef()
+    this.state = {
+      data: []
+    }
   }
 
   componentDidUpdate() {
-    this.myChart.data.labels = this.state.data.map(d => d.label);
-    this.myChart.data.datasets[0].data = this.state.data.map(d => d.value);
-    this.myChart.update();
-    }
+    this.myChart.data.labels = this.state.data.map((d) => d.label)
+    this.myChart.data.datasets[0].data = this.state.data.map((d) => d.value)
+    this.myChart.update()
+  }
 
-   componentDidMount() {
+  componentDidMount() {
     this.myChart = new Chart(this.canvasRef.current, {
-      type: this.props.type ,
+      type: this.props.type,
       options: {
-        legend: { labels: { fontColor: "#ccc", fontSize: 12  }  },
-        plugins: { colorschemes: {  scheme: this.props.scheme  }  } ,
+        legend: { labels: { fontColor: '#ccc', fontSize: 12 } },
+        plugins: { colorschemes: { scheme: this.props.scheme } },
         maintainAspectRatio: false,
-        responsive : true,
+        responsive: true,
         scales: {
-          yAxes: [{  
+          yAxes: [
+            {
               ticks: {
                 min: 0,
                 max: 100
@@ -38,42 +38,37 @@ export default class ChartJS extends React.Component {
         }
       },
       data: {
-        labels: this.state.data.map(d => d.label),
-        datasets: [{
-          label: this.props.title,
-          borderColor : 'rgba(255, 255, 255, 0.5)',
-          borderWidth : 1,
-          data: this.state.data.map(d => d.value),
-        }]
+        labels: this.state.data.map((d) => d.label),
+        datasets: [
+          {
+            label: this.props.title,
+            borderColor: 'rgba(255, 255, 255, 0.5)',
+            borderWidth: 1,
+            data: this.state.data.map((d) => d.value)
+          }
+        ]
       }
-    });
+    })
 
-    this.canvasRef.current.parentNode.style.height = '100%';
-    this.canvasRef.current.parentNode.style.width = '100%';    
+    this.canvasRef.current.parentNode.style.height = '100%'
+    this.canvasRef.current.parentNode.style.width = '100%'
     this.interval = setInterval(() => {
       //console.log(this.props.dsinstid);
       this.setState({
         //data: this.getData(this.props.dsinstid)
-        data : global.dsdata[this.props.dsinstid] ||[]
-      });
-    }, 5000);
+        data: global.dsdata[this.props.dsinstid] || []
+      })
+    }, 5000)
   }
 
   render() {
-        return (
-            <div className= "container"  style = {{width : '100%', height : '90%'}}>
-                <div className="card-body">
-                    <h5 className="mr-auto"> {this.props.title}  </h5>
-                    <canvas ref={this.canvasRef} width="100%" height="200%"/>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className='container' style={{ width: '100%', height: '90%' }}>
+        <div className='card-body'>
+          <h5 className='mr-auto'> {this.props.title} </h5>
+          <canvas ref={this.canvasRef} width='100%' height='200%' />
+        </div>
+      </div>
+    )
+  }
 }
-
-
-
-
-
-
-
--- src/components/widgetLineChart.js
+++ src/components/widgetLineChart.js
@@ -2,31 +2,28 @@ import React from 'react'
 import Chart from 'chart.js'
 import 'chartjs-plugin-colorschemes'
 
-
-
 // LineChart
 export default class WidgetLineChart extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.canvasRef = React.createRef();
+    this.canvasRef = React.createRef()
 
     this.state = {
       //data: this.getData(this.props.dsinstid)
-      data : []
-    };
-    
+      data: []
+    }
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   componentDidUpdate() {
-    this.myChart.data.labels = this.state.data.map(d => d.time);
-    this.myChart.data.datasets[0].data = this.state.data.map(d => d.value);
+    this.myChart.data.labels = this.state.data.map((d) => d.time)
+    this.myChart.data.datasets[0].data = this.state.data.map((d) => d.value)
     // setMode(global.mode);
-    this.myChart.update();
+    this.myChart.update()
   }
 
   componentDidMount() {
@@ -34,8 +31,8 @@ export default class WidgetLineChart extends React.Component {
       type: 'line',
       options: {
         maintainAspectRatio: false,
-        plugins: { colorschemes: {  scheme: this.props.scheme  }  } ,
-        responsive : true,
+        plugins: { colorschemes: { scheme: this.props.scheme } },
+        responsive: true,
         scales: {
           xAxes: [
             {
@@ -45,13 +42,13 @@ export default class WidgetLineChart extends React.Component {
                 unit: 'days',
                 unitStepSize: 1,
                 displayFormats: {
-                   'second': 'ss',
-                   'minute':  'h:mm:ss a'
+                  second: 'ss',
+                  minute: 'h:mm:ss a'
                 }
               },
-             ticks: {
-              beginAtZero: true
-               }
+              ticks: {
+                beginAtZero: true
+              }
             }
           ],
           yAxes: [
@@ -65,45 +62,41 @@ export default class WidgetLineChart extends React.Component {
         }
       },
       data: {
-          labels: this.state.data.map(d => d.time),
-        datasets: [{
-          label: this.props.title,
-          data: this.state.data.map(d => d.value),
-          fill: 'none',
-          backgroundColor: this.props.color,
-          pointRadius: 2,
-          borderColor: this.props.color,
-          borderWidth: 1,
-          lineTension: 0.3
-        }]
+        labels: this.state.data.map((d) => d.time),
+        datasets: [
+          {
+            label: this.props.title,
+            data: this.state.data.map((d) => d.value),
+            fill: 'none',
+            backgroundColor: this.props.color,
+            pointRadius: 2,
+            borderColor: this.props.color,
+            borderWidth: 1,
+            lineTension: 0.3
+          }
+        ]
       }
-    });
+    })
 
-    this.canvasRef.current.parentNode.style.height = '100%';
-    this.canvasRef.current.parentNode.style.width = '100%';
+    this.canvasRef.current.parentNode.style.height = '100%'
+    this.canvasRef.current.parentNode.style.width = '100%'
     this.interval = setInterval(() => {
       // console.log(this.props.dsinstid);
-        this.setState({
-         // data: this.getData(this.props.dsinstid)
-         data : global.dsdata[this.props.dsinstid] ||[]
-        });
-      }, 5000);
-       
+      this.setState({
+        // data: this.getData(this.props.dsinstid)
+        data: global.dsdata[this.props.dsinstid] || []
+      })
+    }, 5000)
   }
 
   render() {
     return (
-        <div className= "container"  style = {{width : '100%', height : '90%'}}>
-            <div className="card-body">
-                <h5 className="mr-auto">Line Chart</h5>
-                <canvas ref={this.canvasRef} width="100%" height="200%"/>
-            </div>
+      <div className='container' style={{ width: '100%', height: '90%' }}>
+        <div className='card-body'>
+          <h5 className='mr-auto'>Line Chart</h5>
+          <canvas ref={this.canvasRef} width='100%' height='200%' />
         </div>
-    );
+      </div>
+    )
   }
 }
-
-
-
-
-
